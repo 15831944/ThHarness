bddbeef1cfe7be0bdc4b654709b9a4652502f4e7
unknown <User@PC191805.thape.com.cn>
2019-07-23 15:49:46 +0800

    .bat sprint

---
+++ ThHarness.bat
@@ -0,0 +1,4 @@
+COPY /Y D:\vsproject\ThHarness\ThHarness\bin\Debug\ThHarness.dll C:\Users\User\.nuget\packages\galliobundle\3.4.14\bin
+SET GALLIO_RUNTIME_PATH=C:\Users\User\.nuget\packages\galliobundle\3.4.14\bin
+"C:\Users\User\.nuget\packages\galliobundle\3.4.14\bin\Gallio.Echo.exe" C:\Users\User\.nuget\packages\galliobundle\3.4.14\bin\ThHarness.dll /r:AutoCAD
+pause
